582144c157df3aae76f3d5c4b387e85f9d025a3a
Wenderson Wanzeller <wenderson@wanzeller.com>
2023-08-23 13:12:11 +0100

    Inclusao de templates

--- galeria/views.py
+++ galeria/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'galeria/index.html')
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
 
