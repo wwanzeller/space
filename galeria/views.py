d04743df5d68c6b81f36b01f03fa9614ae0cfa37
Wenderson Wanzeller <wenderson@wanzeller.com>
2023-08-24 00:14:35 +0100

    Adicionando painel de administracao

--- galeria/views.py
+++ galeria/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
-    fotografias = Fotografia.objects.all()
-    return render(request, 'galeria/index.html', {'cards': fotografias})
+    # o sinal de - em -data inverte a ordem.
+    fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
+
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
-    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
-
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
